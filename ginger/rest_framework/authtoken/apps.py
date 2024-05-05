80d746dff5b02ba01b0314194f72b046690650f5
Ginger Society Admin <admin@gingersociety.org>
2024-05-05 09:33:24 +0000

    chore: removed reference to AUTH_USER for rest_framework

--- ginger/rest_framework/authtoken/apps.py
+++
@@ -1,7 +0,0 @@
-from ginger.apps import AppConfig
-from ginger.utils.translation import gettext_lazy as _
-
-
-class AuthTokenConfig(AppConfig):
-    name = 'rest_framework.authtoken'
-    verbose_name = _("Auth Token")
