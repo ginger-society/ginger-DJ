3ccbf15e2ee5f2b23be20d91ccf1496f7e519d8f
Ginger Society Admin <admin@gingersociety.org>
2024-08-22 01:39:30 +0530

    chore: version bump to 5.3.11

--- ginger/__init__.py
+++ ginger/__init__.py
@@ -1,8 +1,8 @@
 from ginger.utils.version import get_version
 
-VERSION = (5, 3, 10, "final", 0)
+VERSION = (5, 3, 11, "final", 0)
 
-__version__ = (5, 3, 10, "final", 0)
+__version__ = (5, 3, 11, "final", 0)
 
 
 def setup(set_prefix=True):
